8d4e90f8b8e1a84d0d992b6d76268f3d1b4f537f
Genarogg <genarrogg@gmail.com>
2020-04-01 17:15:19 -0400

    estructura del password

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,11 +1,24 @@
 import React from "react";
 
 const Login = () => {
+  const onChange = () => {
+    /*  */
+  };
   return (
     <div className="from-usuario">
       <div className="contenedor-form sombra-dark">
         <h1>Iniciar Seción</h1>
         <form action="">
+          <div className="compo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Tu Email"
+              onChange={onChange}
+            />
+          </div>
           <div className="compo-form">
             <label htmlFor="email">Email</label>
             <input
