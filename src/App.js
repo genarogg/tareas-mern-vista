921656af79ef08e545de3b6040258c3877370222
Genarogg <genarrogg@gmail.com>
2020-04-01 16:44:53 -0400

    imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Login from "./components/auth/Login"
+import NuevaCuenta from "./components/auth/NuevaCuenta"
+import Proyectos from "./components/proyectos/Proyectos"
 
 function App() {
   return (
