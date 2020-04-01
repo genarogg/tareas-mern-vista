f9f1f37baf96ee224bc6711caaee97c38aa49c50
Genarogg <genarrogg@gmail.com>
2020-04-01 16:45:00 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 import App from './App';
 
 
-import "./css/normalize.css"
-import "./css/style.css"
+import "./css/normalize.css";
+import "./css/style.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
