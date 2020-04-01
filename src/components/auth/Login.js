b6f0e1ae38005785e0d8872295b47f2039be33f7
Genarogg <genarrogg@gmail.com>
2020-04-01 17:43:46 -0400

    obteniendo parametros de los input

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  /* State para iniciar Sesión */
+  const [usuario, guardarUsuario] = useState({
+    email: "",
+    password: "",
+  });
 
-    /* State para iniciar Sesión */
-    const [usuario, guardar] = useState({
-        email: "",
-        password: ""
-    })
-  const onChange = () => {
-    /*  */
+  /* Extraer Usuario */
+  const { email, password } = usuario;
+  const onChange = (e) => {
+    guardarUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
   };
   return (
     <div className="form-usuario">
@@ -22,6 +27,7 @@ const Login = () => {
               name="email"
               id="email"
               placeholder="Tu Email"
+              value={email}
               onChange={onChange}
             />
           </div>
@@ -32,6 +38,7 @@ const Login = () => {
               name="password"
               id="password"
               placeholder="Tu Password"
+              value={password}
               onChange={onChange}
             />
           </div>
