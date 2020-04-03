ebce067d156e2e39e2653c6079cbb31ed061d32c
Genarogg <genarrogg@gmail.com>
2020-04-02 21:15:45 -0400

    State para iniciar Sesión

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -2,7 +2,17 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 const NuevaCuenta = () => {
   /* State para iniciar Sesión */
+  const [usuario, guardarUsuario] = useState({
+    nombre: "",
+    email: "",
+    password: "",
+    confirmar: "",
+  });
+
+  /* Extraer Usuario */
+  const { nombre, email, password, confirmar } = usuario;
   
+
   return (
     <div className="form-usuario">
       <div className="contenedor-form sombra-dark">
