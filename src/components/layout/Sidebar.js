c5115ce92691ef1fc2a5edb4f20203e2c18703e2
Genarogg <genarrogg@gmail.com>
2020-04-02 22:34:13 -0400

    Sidebar

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sidebar = ()=>{
+    return(<aside>
+        <h1>MERN <span>Tasks</span></h1>
+        <div className="proyectos">
+            <h2>Tus Proyectos</h2>
+        </div>
+    </aside>)
+}
+
+export default Sidebar;
