b50f033017b08df0c94a73f93f134fd609ca6dcf
Genarogg <genarrogg@gmail.com>
2020-04-04 16:22:05 -0400

    ListadoTareas

---
+++ src/components/tareas/ListadoTareas.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from "react"
+
+const ListadoTareas = () => {
+
+    return(<Fragment>
+        <h2>Proyecto: Tienda Virtual</h2>
+        <ul className="listado-tareas"></ul>
+    </Fragment>)
+}
+
+export default ListadoTareas;
