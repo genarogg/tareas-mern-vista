c5a4fbef1218d47f1b3629ba2c2a5cc8337eb225
Genarogg <genarrogg@gmail.com>
2020-04-03 23:32:09 -0400

    Sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import NuevoProyecto from "../proyectos/NuevoProyecto"
 
 const Sidebar = ()=>{
     return(<aside>
         <h1>MERN <span>Tasks</span></h1>
+        <NuevoProyecto/>
         <div className="proyectos">
             <h2>Tus Proyectos</h2>
         </div>
