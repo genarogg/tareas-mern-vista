4ec630cb3ea9f875bd8d4f1a53e657422baa6880
Genarogg <genarrogg@gmail.com>
2020-04-04 12:49:57 -0400

    [proyecto, guardarProyecto]

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -1,15 +1,31 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const NuevoProyecto = () => {
+  /* State para el Proyecto */
+  const [proyecto, guardarProyecto] = (e) => {
+    guardarProyecto({
+      ...proyecto,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <Fragment>
-        <button type="button" className="btn btn-block btn-primario">
-      Nuevo Proyecto
-    </button>
-    <form action="" className="formulario-nuevo-proyecto">
-        <input type="text" className="input-text" placeholder="Nombre Proyecto" name="nombre"/>
-        <input type="submit" className="btn btn-primario btn-block" value="Agregar Proyecto"/>
-    </form>
+      <button type="button" className="btn btn-block btn-primario">
+        Nuevo Proyecto
+      </button>
+      <form action="" className="formulario-nuevo-proyecto">
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Nombre Proyecto"
+          name="nombre"
+        />
+        <input
+          type="submit"
+          className="btn btn-primario btn-block"
+          value="Agregar Proyecto"
+        />
+      </form>
     </Fragment>
   );
 };
