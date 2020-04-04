cfa250ec30a2b34ae0c01ed1016405b72f920176
Genarogg <genarrogg@gmail.com>
2020-04-04 16:16:02 -0400

    fix

--- src/components/proyectos/Proyectos.js
+++ src/components/proyectos/Proyectos.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Sidebar from "../layout/Sidebar"
 import Barra from "../layout/Barra"
+import FormTarea from  "../tareas/FormTarea"
 
 const Proyectos = () => {
     return (<div className="contenedor-app">
@@ -8,6 +9,7 @@ const Proyectos = () => {
         <div className="seccion-principal">
             <Barra />
             <main>
+                <FormTarea/>
                 <div className="contenedor-tareas"></div>
             </main>
         </div>
