2481d4e4a864ef92a58b7ee642ff2c0ea111e878
Genarogg <genarrogg@gmail.com>
2020-04-04 16:28:41 -0400

    tareasProyecto

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -2,9 +2,20 @@ import React, {Fragment} from "react"
 
 const ListadoTareas = () => {
 
+    const tareasProyecto = [
+        {nombre: "Elegir Plataforma", estado: true},
+        {nombre: "Elegir Colores", estado: false},
+        {nombre: "Elegir Plataformas de pago", estado: false},
+        {nombre: "Elegir Hosting", estado: true}
+    ]
+
     return(<Fragment>
         <h2>Proyecto: Tienda Virtual</h2>
-        <ul className="listado-tareas"></ul>
+        <ul className="listado-tareas">
+            {tareas.length === 0
+            ? (<li className="tarea"><p>No hay tareas</p></li>) 
+            : {tareasProyecto.map(tarea => (<Tarea tarea={tarea}/>))}}
+        </ul>
     </Fragment>)
 }
 
