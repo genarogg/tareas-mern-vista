12d691be115b992086827ee74fc88b9044c57cb6
Genarogg <genarrogg@gmail.com>
2020-04-04 13:20:09 -0400

    Cuenado el usuario envia un proyecto

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -2,28 +2,47 @@ import React, { Fragment, useState } from "react";
 
 const NuevoProyecto = () => {
   /* State para el Proyecto */
-  const [proyecto, guardarProyecto] = useState => ({
-    nombre: ""
+  const [proyecto, guardarProyecto] = useState({
+    nombre: "",
   });
 
-  const onchangeProyecto = e => {
+  /* Extraer nombre de proyecto */
+  const { nombre } = proyecto;
+/* Lee los contenidos del input */
+  const onChangeProyecto = (e) => {
     guardarProyecto({
       ...proyecto,
       [e.target.name]: e.target.value,
     });
+  };
 
+  /* Cuenado el usuario envia un proyecto */
+  const onSubmitProyecto = e => {
+    e.preventDefault();
+
+    /* Validar el proyecto */
+
+    /* Agregar al state */
+
+    /* Reiniciar el form */
   }
   return (
     <Fragment>
       <button type="button" className="btn btn-block btn-primario">
         Nuevo Proyecto
       </button>
-      <form action="" className="formulario-nuevo-proyecto">
+      <form
+        action=""
+        className="formulario-nuevo-proyecto"
+        onSubmit={onSubmitProyecto}
+      >
         <input
           type="text"
           className="input-text"
           placeholder="Nombre Proyecto"
           name="nombre"
+          value={nombre}
+          onChange={onChangeProyecto}
         />
         <input
           type="submit"
