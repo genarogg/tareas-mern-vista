60f98070273e3eadd2212ff44dd10b807f5fd57e
Genarogg <genarrogg@gmail.com>
2020-04-06 17:16:44 -0400

    botones de eliminar y editar

--- src/components/tareas/Tarea.js
+++ src/components/tareas/Tarea.js
@@ -1,20 +1,23 @@
 import React from "react";
 
-const Tarea = ({tarea}) => {
+const Tarea = ({ tarea }) => {
   return (
     <li className="tarea sombra">
       <p>{tarea.nombre}</p>
       <div className="estado">
-        {tarea.estado
-        ?(
-        <button type="button" className="completo">
-          Completo
-        </button>
-        ): (
-        <button type="button" className="incompleto">
-          InCompleto
-        </button>
-        ) }
+        {tarea.estado ? (
+          <button type="button" className="completo">
+            Completo
+          </button>
+        ) : (
+          <button type="button" className="incompleto">
+            InCompleto
+          </button>
+        )}
+      </div>
+      <div className="acciones">
+        <button type="button" className="btn btn-primario">Editar</button>
+        <button type="button" className="btn btn-secundario">Eliminar</button>
       </div>
     </li>
   );
