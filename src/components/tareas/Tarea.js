726753f64e6bab2c54c9eedf433afd0bbf7337d8
Genarogg <genarrogg@gmail.com>
2020-04-06 17:07:18 -0400

    Tarea

--- src/components/tareas/Tarea.js
+++ src/components/tareas/Tarea.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Tarea = () => {
-    return ( <p>Hola desde tarea</p> );
-}
- 
+  return (
+    <li className="tarea sombra">
+      <p>{Tarea.nombre}</p>
+      <div className="estado">
+        {Tarea.estado} ? ({" "}
+        <button type="button" className="completo">
+          Completo
+        </button>
+        )
+      </div>
+    </li>
+  );
+};
+
 export default Tarea;
