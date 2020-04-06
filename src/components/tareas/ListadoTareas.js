e8d17ae343b2caf93efa808bfaea8c628486b918
Genarogg <genarrogg@gmail.com>
2020-04-06 17:19:38 -0400

    botton de eliminar tareas

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,23 +1,31 @@
-import React, {Fragment} from "react"
-import Tarea from "../tareas/Tarea"
+import React, { Fragment } from "react";
+import Tarea from "../tareas/Tarea";
 
 const ListadoTareas = () => {
+  const tareasProyecto = [
+    { nombre: "Elegir Plataforma", estado: true },
+    { nombre: "Elegir Colores", estado: false },
+    { nombre: "Elegir Plataformas de pago", estado: false },
+    { nombre: "Elegir Hosting", estado: true },
+  ];
 
-    const tareasProyecto = [
-        {nombre: "Elegir Plataforma", estado: true},
-        {nombre: "Elegir Colores", estado: false},
-        {nombre: "Elegir Plataformas de pago", estado: false},
-        {nombre: "Elegir Hosting", estado: true}
-    ]
-
-    return(<Fragment>
-        <h2>Proyecto: Tienda Virtual</h2>
-        <ul className="listado-tareas">
-            {tareasProyecto.length === 0
-            ? (<li className="tarea"><p>No hay tareas</p></li>) 
-            : tareasProyecto.map(tarea => (<Tarea tarea={tarea}/>))}
-        </ul>
-    </Fragment>)
-}
+  return (
+    <Fragment>
+      <h2>Proyecto: Tienda Virtual</h2>
+      <ul className="listado-tareas">
+        {tareasProyecto.length === 0 ? (
+          <li className="tarea">
+            <p>No hay tareas</p>
+          </li>
+        ) : (
+          tareasProyecto.map((tarea) => <Tarea tarea={tarea} />)
+        )}
+      </ul>
+      <button type="button" className="btn btn-eliminar">
+        Eliminar Proyecto &times;
+      </button>
+    </Fragment>
+  );
+};
 
 export default ListadoTareas;
