9f3b96591e5e26f09b47b06bb1a19b0e4880ca88
Genarogg <genarrogg@gmail.com>
2020-04-08 13:07:08 -0400

    FORMULARIO_PROYECTO

--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -3,16 +3,25 @@ import React, { useReducer } from "react";
 import proyectoContext from "./proyectoContext";
 import proyectoReducer from "./proyectoReducer";
 
+import { FORMULARIO_PROYECTO } from "../../types";
+
 const ProyectoState = (props) => {
   const initialState = {
-    formulario: false
+    formulario: false,
   };
   /* Dispatch para ejecutar las acciones */
   const [state, dispatch] = useReducer(proyectoReducer, initialState);
 
   /* Serie de funciones para el CRUD */
+  const mostrarFormulario = () => {
+    dispatch({
+      type: FORMULARIO_PROYECTO,
+    });
+  };
   return (
-    <proyectoContext.Provider value={{ formulario: state.formulario }}>
+    <proyectoContext.Provider
+      value={{ formulario: state.formulario, mostrarFormulario }}
+    >
       {props.children}
     </proyectoContext.Provider>
   );
