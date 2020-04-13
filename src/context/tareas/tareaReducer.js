8a572744c3bf32de292e5fac40cbe8043adb1f9e
Genarogg <genarrogg@gmail.com>
2020-04-13 17:52:43 -0400

    ESTADO_TAREA

--- src/context/tareas/tareaReducer.js
+++ src/context/tareas/tareaReducer.js
@@ -3,6 +3,7 @@ import {
   AGREGAR_TAREA,
   VALIDAR_TAREA,
   ELIMINAR_TAREA,
+  ESTADO_TAREA
 } from "../../types";
 
 export default (state, action) => {
@@ -31,6 +32,11 @@ export default (state, action) => {
         ...state,
         tareas: state.tareas.filter((tarea) => tarea.id !== action.payload),
       };
+    case ESTADO_TAREA:
+      return{
+        ...state,
+        tareas: state.tareasproyecto.map(tarea => tarea.id === action.payload.id ? action.payload :tarea)
+      }
     default:
       return state;
   }
