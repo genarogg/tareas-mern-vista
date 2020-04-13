b14603752c8d48e5af4d9344a41197bcfa291287
Genarogg <genarrogg@gmail.com>
2020-04-13 17:42:54 -0400

    cambiarEstadoTarea

--- src/context/tareas/tareaState.js
+++ src/context/tareas/tareaState.js
@@ -6,6 +6,7 @@ import {
   AGREGAR_TAREA,
   VALIDAR_TAREA,
   ELIMINAR_TAREA,
+  ESTADO_TAREA
 } from "../../types";
 
 const TareaState = (props) => {
@@ -64,6 +65,15 @@ const TareaState = (props) => {
       payload: id
     })
   }
+
+  /* Cambia el estado de cada tarea */
+  const cambiarEstadoTarea = tarea => {
+    dispatch({
+      type: ESTADO_TAREA,
+      payload:tarea
+    })
+  }
+
   return (
     <TareaContext.Provider
       value={{
@@ -73,7 +83,8 @@ const TareaState = (props) => {
         obtenerTareas,
         agregarTarea,
         validarTarea,
-        eliminarTarea
+        eliminarTarea,
+        cambiarEstadoTarea
       }}
     >
       {props.children}
