de7e77693fde31aa10fb3ce53629a46c457459db
genaro gonzalez <genarrogg@gmail.com>
2022-10-07 00:50:47 -0400

    link

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const clienteAxios = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: 'https://mern-task-servidor.herokuapp.com/',
 });
 
 export default clienteAxios
